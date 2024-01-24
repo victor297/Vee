7998df9fad9c7e932d64a67b764ef2a33bc60b09
ireoluwa victor <38535954+victor297@users.noreply.github.com>
2024-01-24 20:28:00 +0100

    Update Header.jsx

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
             <span>👋</span>
             <div style={{ marginLeft: 20 }}>
               <p className='p-text'>Hello, I am</p>
-              <h1 className='head-text'>Ogunyinka ireoluwa Victor</h1>
+              <h1 className='head-text'>David Victor</h1>
             </div>
           </div>
           <div className='tag-cmp app__flex'>
@@ -41,7 +41,7 @@ const Header = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__header-img"
       >
-{/*         <img src={images.profile} alt="profile_bg" /> */}
+       <img src={images.profile} alt="profile_bg" /> 
         <motion.img
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: 'easeInOut' }}
